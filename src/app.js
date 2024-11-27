225b1986e1d4ce57c3381b315fc763c2daa39901
shubham patil <shubspatil17@gmail.com>
2024-11-27 16:05:44 +0530

    Testing GET, POST and other api

--- src/app.js
+++ src/app.js
@@ -2,17 +2,37 @@ const express = require("express");
 
 const app = express();
 
+app.get("/user" , (req, res) => {
+    res.send("Handling GET API handler");
+});
+
+app.post("/user" , (req, res) => {
+    res.send("Handling POST API handler");
+});
+
+app.delete("/user" , (req, res) => {
+    res.send("Handling DELETE API handler");
+});
+
+app.put("/user" , (req, res) => {
+    res.send("Handling PUT API handler");
+});
+
+app.patch("/user" , (req, res) => {
+    res.send("Handling PATCH API handler");
+});
+
 app.use("/test", (req, res) => {
     res.send("Handling response on dashbord");
 });
 
-app.use("/about" , (req, res) => {
-    res.send("Shubham patil");
-});
+// app.use("/about" , (req, res) => {
+//     res.send("Shubham patil");
+// });
 
-app.use("/contact" , (req, res) => {
-    res.send("8861422199");
-});
+// app.use("/contact" , (req, res) => {
+//     res.send("8861422199");
+// });
 
 app.listen(7777, () => {
     console.log("Hey your server is listening on 7777...");
